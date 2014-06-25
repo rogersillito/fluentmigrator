75a02f45c3c957999c36cf20e520061b6263615e
rogersillito <rogersillito@gmail.com>
2014-06-25 10:56:06 +0100

    added script to do a release build

---
+++ build-release.bat
@@ -0,0 +1,3 @@
+tools\nant\nant.exe -buildfile:default.build -D:build.number=0 -D:build.config=release -D:build.platform=net-3.5 %*
+@REM tools\nant\nant.exe -buildfile:default.build -D:build.number=0 -D:build.config=release -D:build.platform=net-4.0 %*
+pause
